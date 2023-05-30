ba93a786c1fcdf3ae47a81eec0017c26243cb194
listingslab <listingslab@gmail.com>
2023-05-30 13:11:09 +0200

    updates tests (#17)

--- react-app/src/BitcoinHash/BitcoinHash.test.tsx
+++ react-app/src/BitcoinHash/BitcoinHash.test.tsx
@@ -34,5 +34,5 @@ test('Renders app', () => {
       <BitcoinHash />
     </Provider>
   );
-  expect(getByText(/Bitcoin#/i)).toBeInTheDocument();
+  expect(getByText(/Blockchain/i)).toBeInTheDocument();
 });
